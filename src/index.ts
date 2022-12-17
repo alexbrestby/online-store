f5496c25b2d55c315434ca03d24e29289eb9e9c0
Alex Leoniuk <alex.brest.by@gmail.com>
2022-12-17 17:49:29 +0300

    feat: sliders add

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import './global.css';
 import { categoryFilter } from './components/category-filter/index';
 import { brandFilter } from './components/brand-filter/index';
+import { priceSlider } from './components/price-slider/index';
+import { stockSlider } from './components/stock-slider/index';
 
 categoryFilter();
 brandFilter();
+priceSlider();
+stockSlider();
