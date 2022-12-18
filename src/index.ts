f9e836f442876bdda73b594e30cbb4225e930c8f
Alex Leoniuk <alex.brest.by@gmail.com>
2022-12-18 14:04:02 +0300

    feat: render-area-design add

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import { categoryFilter } from './components/category-filter/index';
 import { brandFilter } from './components/brand-filter/index';
 import { priceSlider } from './components/price-slider/index';
 import { stockSlider } from './components/stock-slider/index';
+import productsRender from './components/products-render/index';
 
 categoryFilter();
 brandFilter();
 priceSlider();
 stockSlider();
+productsRender();
