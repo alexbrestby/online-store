ad0d7930fe6f3b496de7a92e0cb9b3bc0041eb23
PavelPoleshchuk <ppl@vivanta.by>
2022-12-20 21:58:18 +0300

    feat: add buttons,localStorage-basket, quantity of ordered goods in header

--- src/components/products-render/index.ts
+++ src/components/products-render/index.ts
@@ -1,35 +1,71 @@
 import './style.css';
+interface IbasketRender {
+  [ind: string]: number;
+}
 
-const productsRender = () => {
+const basketRender: IbasketRender = {};
+const totalItemInBasket = <HTMLElement>document.querySelector('.total-item');
+// totalItemInBasket.textContent ='111';
+
+export const productsRender = () => {
   const render = document.querySelector('.render-area');
+  
 
   // подтягиваем список товаров
-  const base:
-    {
-      id: number,
-      title: string,
-      description: string,
-      price: number,
-      discountPercentage: number,
-      rating: number,
-      stock: null,
-      brand: string,
-      category: string,
-      thumbnail: string,
-      images: string[]
-    }[] = [];
+  const base: {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    discountPercentage: number;
+    rating: number;
+    stock: null;
+    brand: string;
+    category: string;
+    thumbnail: string;
+    images: string[];
+  }[] = [];
 
   fetch('https://dummyjson.com/products?limit=48')
     .then((res) => res.json())
     .then((data) => {
       for (const product of data.products) {
-
         base.push(product);
 
         const item = document.createElement('div');
         item.classList.add('product-item');
 
         item.dataset.id = product.id;
+
+        const itemButtonsWrapper = document.createElement('div');
+        itemButtonsWrapper.classList.add('item-buttons-wrapper');
+
+        const itemWrapperButtonBuy = document.createElement('button');
+        itemWrapperButtonBuy.classList.add('item-wrapper-button-buy');
+        itemWrapperButtonBuy.classList.add('button');
+        itemWrapperButtonBuy.dataset.buy = product.id;
+        itemWrapperButtonBuy.textContent = 'добавить';
+
+        itemWrapperButtonBuy.addEventListener('click', () => {
+          let idIndex = itemWrapperButtonBuy.dataset.buy;
+          if (basketRender[idIndex!] !== 1) {
+            basketRender[idIndex!] = 1;
+            itemWrapperButtonBuy.textContent = 'удалить';
+          } else {
+            basketRender[idIndex!]--;
+            itemWrapperButtonBuy.textContent = 'добавить';
+          }
+          totalItemInBasket.textContent = ''+getNonNullKeys(basketRender);
+          localStorage.setItem('basket', JSON.stringify(basketRender));
+        });
+
+        const itemWrapperButtonInfo = document.createElement('button');
+        itemWrapperButtonInfo.classList.add('item-wrapper-button-info');
+        itemWrapperButtonInfo.classList.add('button');
+        itemWrapperButtonInfo.dataset.info = product.id;
+        itemWrapperButtonInfo.textContent = 'подробно';
+
+        itemButtonsWrapper.append(itemWrapperButtonBuy, itemWrapperButtonInfo);
 
         const itemWrapper = document.createElement('div');
         itemWrapper.classList.add('item-wrapper');
@@ -38,14 +74,23 @@ const productsRender = () => {
         const title = document.createElement('h2');
         title.innerHTML = `${product.title}`;
 
+        itemWrapper.append(itemButtonsWrapper);
         item.append(title);
         item.append(itemWrapper);
         render?.append(item);
       }
     })
-    .catch(e => console.log((e)));
+    .catch((e) => console.log(e));
 
   return base;
 };
 
-export default productsRender;
+function getNonNullKeys(obj: IbasketRender) {
+  let quantity = 0;
+  for (let item of Object.values(obj)) {
+    if (item > 0) {
+      quantity++;
+    }
+  }
+  return quantity;
+}
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { categoryFilter } from './components/category-filter/index';
 import { brandFilter } from './components/brand-filter/index';
 import { priceSlider } from './components/price-slider/index';
 import { stockSlider } from './components/stock-slider/index';
-import productsRender from './components/products-render/index';
+import {productsRender} from './components/products-render/index';
 import { globalSearch } from './components/global-search/index';
 
 
