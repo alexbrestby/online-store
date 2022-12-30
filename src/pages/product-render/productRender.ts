886fc37888e256c109fab06b026bf584772bdb0b
Alex Leoniuk <alex.brest.by@gmail.com>
2022-12-30 16:34:33 +0300

    feat: single product page add

--- src/pages/product-render/productRender.ts
+++ src/pages/product-render/productRender.ts
@@ -1,5 +1,6 @@
 import './product-render.css';
 import { HistoryState, Iproduct } from '../../types/types';
+import { sliders } from '../main-render/sliders/sliders';
 
 const productRender = () => {
   (document.querySelector('.header-search') as HTMLElement).style.display = 'none';
@@ -38,11 +39,85 @@ const productRender = () => {
       const titleCrumb = document.createElement('div');
       titleCrumb.innerHTML = data.title;
       breadcrumbWrapper.append(titleCrumb);
+
+      const productTitle = document.createElement('div');
+      productTitle.classList.add('product-title');
+
+      const productTitleH1 = document.createElement('div');
+      productTitleH1.innerHTML = data.title;
+
+      const productData = document.createElement('div');
+      productData.classList.add('product-data');
+
+      const productPhotos = document.createElement('div');
+      productPhotos.classList.add('product-photos');
+
+      const slides = document.createElement('div');
+      slides.classList.add('slides');
+
+      let imagesCount;
+      data.images.length > 4 ? imagesCount = 4 : imagesCount = data.images.length
+      for (let i = 0; i < imagesCount; i++) {
+        const slide = document.createElement('img');
+        if (data.images[i] === data.thumbnail) {
+          slide.classList.add('active');
+        }
+        slide.alt = `${data.title}-${[i]}`;
+        slide.src = data.images[i];
+        slides.append(slide);
+      }
+
+      const grandPhoto = document.createElement('div');
+      grandPhoto.classList.add('grand-photo');
+
+      const grandPhotoImg = document.createElement('img');
+      grandPhotoImg.src = data.thumbnail;
+      grandPhotoImg.alt = data.title;
+      grandPhoto.append(grandPhotoImg);
+
+      productPhotos.append(slides, grandPhoto);
+
+      const productInfo = document.createElement('div');
+      productInfo.classList.add('product-info');
+
+      for (let i = 2; i < 9; i++) {
+        const property = document.createElement('div');
+        property.classList.add('property');
+
+        const propertyh3 = document.createElement('h3');
+        propertyh3.innerHTML = Object.keys(data)[i];
+
+        const propertyP = document.createElement('p');
+        propertyP.innerHTML = data[Object.keys(data)[i]] as string;
+
+        property.append(propertyh3, propertyP);
+        productInfo.append(property);
+      }
+
+      const productAddToCart = document.createElement('div');
+      productAddToCart.classList.add('add-to-cart');
+
+      const bigPrice = document.createElement('div');
+      bigPrice.classList.add('big-price');
+      bigPrice.innerHTML = `&#8364; ${data.price}`;
+
+      const addToCartButton = document.createElement('div');
+      addToCartButton.classList.add('button', 'add-button')
+      addToCartButton.innerHTML = `Add To Cart`;
+
+      const buyNowButton = document.createElement('div');
+      buyNowButton.classList.add('button', 'buy-button')
+      buyNowButton.innerHTML = `Buy Now`;
+
+      productAddToCart.append(bigPrice, addToCartButton, buyNowButton);
+
+      productData.append(productPhotos, productInfo, productAddToCart);
+      productTitle.append(productTitleH1);
+      productWrapper.append(productTitle, productData);
     })
     .catch((e) => console.log(e));
 
-  productDetailPage.append(breadcrumbWrapper);
-  productDetailPage.append(productWrapper);
+  productDetailPage.append(breadcrumbWrapper, productWrapper);
   renderArea.append(productDetailPage);
 };
 
